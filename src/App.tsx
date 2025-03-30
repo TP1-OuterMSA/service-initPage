9fdb8edd36ff544f77b72d4a8a7d19c2edf88763
Lim Seongje <96779047+seongje00416@users.noreply.github.com>
2025-03-30 12:04:16 +0900

    feat: Init Page

---
+++ src/App.tsx
@@ -0,0 +1,53 @@
+import {BottomContainer, LinkBubble, MainContainer, MiddleContainer, TopContainer} from "./mainStyle.ts";
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  
+  body, html {
+    width: 100%;
+    height: 100%;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+  }
+`;
+
+function App() {
+
+  return (
+      <>
+      <GlobalStyle />
+        <MainContainer>
+          <TopContainer />
+          <MiddleContainer>
+            <LinkBubble color={"darkblue"}>
+              서비스 1팀
+            </LinkBubble>
+            <LinkBubble color={"green"}>
+              서비스 2팀
+            </LinkBubble>
+            <LinkBubble color={"blue"}>
+              서비스 3팀
+            </LinkBubble>
+            <LinkBubble color={"darkgreen"}>
+              서비스 4팀
+            </LinkBubble>
+            <LinkBubble color={"green"}>
+              서비스 5팀
+            </LinkBubble>
+            <LinkBubble color={"brown"}>
+              서비스 6팀
+            </LinkBubble>
+          </MiddleContainer>
+          <BottomContainer />
+        </MainContainer>
+      </>
+  )
+}
+
+export default App
---
+++ src/mainStyle.ts
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+
+export const MainContainer = styled.div`
+    width: 100vw;
+    height: 100vh;
+`
+
+export const TopContainer = styled.div`
+    width: 100vw;
+    height: 50px;
+    background-color: darkblue;
+`
+export const MiddleContainer = styled.div`
+    width: 100%;
+    height: calc( 100vh - 150px );
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-image: url('../public/mju_logo.png');
+    background-repeat: no-repeat;
+    background-position: center center;
+    background-size: 35%;
+    gap: 5%;
+`
+
+export const LinkBubble = styled.div`
+    border-radius: 100%;
+    background-color: ${props => props.color };
+    width: 10%;
+    aspect-ratio: 1 / 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    opacity: 0.85;
+    color: white;
+    font-size: 1.2rem;
+    
+    &:hover {
+        cursor: pointer;
+        opacity: 1;
+    }
+`
+export const BottomContainer = styled.div`
+    width: 100vw;
+    height: 100px;
+    background-color: black;
+`
