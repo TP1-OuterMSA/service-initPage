3e0ab3ee85d759f5e23289e604b385bbe169daec
seongje00416 <seongje00416@gmail.com>
2025-05-08 14:50:47 +0900

    feat: Main 페이지 디자인 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,26 @@
-import {BottomContainer, LinkBubble, MainContainer, MiddleContainer, TopContainer} from "./mainStyle.ts";
+import {
+  BackgroundImage,
+  BlockContent,
+  BlockTitle,
+  Button,
+  ButtonWrapper,
+  ContentBlock,
+  ContentBlockLine,
+  ContentContainer,
+  ContentLongBlock,
+  ContentTodayInformationBlock,
+  LeftCategoryItemIcon,
+  LeftCategoryItemName,
+  LeftCategoryItemsWrapper,
+  LeftContainer,
+  LeftLogoWrapper,
+  MainContainer,
+  MiddleContainer,
+  OuterContainer,
+  TodayFoodWrapper,
+  TodayLeftWrapper,
+  TopContainer
+} from "./mainStyle.ts";
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
@@ -23,28 +45,72 @@ function App() {
       <>
       <GlobalStyle />
         <MainContainer>
-          <TopContainer />
+          <LeftContainer>
+            <LeftLogoWrapper> 학식 서비스 </LeftLogoWrapper>
+            <LeftCategoryItemsWrapper onClick={ () => window.location.href = '/team4' }>
+              <LeftCategoryItemIcon src="/list.svg" />
+              <LeftCategoryItemName> 오늘의 학식 </LeftCategoryItemName>
+            </LeftCategoryItemsWrapper>
+            <LeftCategoryItemsWrapper onClick={ () => window.location.href = '/team5' }>
+              <LeftCategoryItemIcon src="/board.svg" />
+              <LeftCategoryItemName> 학식에 대해 </LeftCategoryItemName>
+            </LeftCategoryItemsWrapper>
+            <LeftCategoryItemsWrapper onClick={ () => window.location.href = '/team2' }>
+              <LeftCategoryItemIcon src="/community.svg" />
+              <LeftCategoryItemName> 자유 게시판 </LeftCategoryItemName>
+            </LeftCategoryItemsWrapper>
+            <LeftCategoryItemsWrapper onClick={ () => window.location.href = '/team6' }>
+              <LeftCategoryItemIcon src="/person.svg" />
+              <LeftCategoryItemName> 나만의 학식 </LeftCategoryItemName>
+            </LeftCategoryItemsWrapper>
+            <LeftCategoryItemsWrapper onClick={ () => window.location.href = '/team3' }>
+              <LeftCategoryItemIcon src="/chart.svg" />
+              <LeftCategoryItemName> 통계 관리 </LeftCategoryItemName>
+            </LeftCategoryItemsWrapper>
+            <LeftCategoryItemsWrapper onClick={ () => window.location.href = '/team1' }>
+              <LeftCategoryItemIcon src="/setting.svg" />
+              <LeftCategoryItemName> 식당 관리 </LeftCategoryItemName>
+            </LeftCategoryItemsWrapper>
+          </LeftContainer>
           <MiddleContainer>
-            <LinkBubble color={"darkblue"} onClick={ () => window.location.href = '/team1' }>
-              서비스 1팀
-            </LinkBubble>
-            <LinkBubble color={"green"} onClick={ () => window.location.href = '/team2' }>
-              서비스 2팀
-            </LinkBubble>
-            <LinkBubble color={"blue"} onClick={ () => window.location.href = '/team3' }>
-              서비스 3팀
-            </LinkBubble>
-            <LinkBubble color={"darkgreen"} onClick={ () => window.location.href = '/team4' }>
-              서비스 4팀
-            </LinkBubble>
-            <LinkBubble color={"green"} onClick={ () => window.location.href = '/team5' }>
-              서비스 5팀
-            </LinkBubble>
-            <LinkBubble color={"brown"} onClick={ () => window.location.href = '/team6' }>
-              서비스 6팀
-            </LinkBubble>
+            <TopContainer>
+              <ButtonWrapper>
+                <Button> 로그인 </Button>
+              </ButtonWrapper>
+            </TopContainer>
+            <OuterContainer>
+              <BackgroundImage />
+              <ContentContainer>
+                <ContentTodayInformationBlock>
+                  2025년 05월 20일
+                </ContentTodayInformationBlock>
+                <ContentBlockLine>
+                  <ContentBlock>
+                    <BlockTitle> 오늘의 학식 </BlockTitle>
+                    <BlockContent>
+                      <TodayFoodWrapper> 흰쌀밥 </TodayFoodWrapper>
+                      <TodayFoodWrapper> 소고기미역국 </TodayFoodWrapper>
+                      <TodayFoodWrapper> 찹쌀탕수육 </TodayFoodWrapper>
+                      <TodayFoodWrapper> 배추김치 </TodayFoodWrapper>
+                      <TodayFoodWrapper> 시금치무침 </TodayFoodWrapper>
+                      <TodayFoodWrapper> 간장소불고기 </TodayFoodWrapper>
+                    </BlockContent>
+                  </ContentBlock>
+                  <ContentBlock>
+                    <BlockTitle> 남은 식권 개수 </BlockTitle>
+                    <BlockContent>
+                      <TodayLeftWrapper> 1 </TodayLeftWrapper>
+                    </BlockContent>
+                  </ContentBlock>
+                </ContentBlockLine>
+                <ContentBlockLine>
+                  <ContentLongBlock>
+                    <BlockTitle> 최근 게시글 </BlockTitle>
+                  </ContentLongBlock>
+                </ContentBlockLine>
+              </ContentContainer>
+            </OuterContainer>
           </MiddleContainer>
-          <BottomContainer />
         </MainContainer>
       </>
   )
--- src/mainStyle.ts
+++ src/mainStyle.ts
@@ -3,45 +3,177 @@ import styled from "styled-components";
 export const MainContainer = styled.div`
     width: 100vw;
     height: 100vh;
-`
-
-export const TopContainer = styled.div`
-    width: 100vw;
-    height: 50px;
-    background-color: darkblue;
+    display: flex;
 `
 export const MiddleContainer = styled.div`
-    width: 100%;
-    height: calc( 100vh - 150px );
     display: flex;
-    justify-content: center;
-    align-items: center;
-    background-image: url('/mju_logo.png');
-    background-repeat: no-repeat;
-    background-position: center center;
+    flex-direction: column;
+    width: 80%;
+    background-color: whitesmoke;
     background-size: 35%;
     gap: 5%;
 `
 
-export const LinkBubble = styled.div`
-    border-radius: 100%;
-    background-color: ${props => props.color };
-    width: 10%;
-    aspect-ratio: 1 / 1;
+// 좌측 메뉴 카테고리 컨테이너
+export const LeftContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 10px;
+    width: 20%;
+    background-color: white;
+    border-right: 1px solid lightgray;
+`
+export const LeftLogoWrapper = styled.div`
+    font-size: 1.7rem;
+    font-weight: bold;
     display: flex;
     justify-content: center;
     align-items: center;
-    opacity: 0.85;
-    color: white;
+    padding: 15px;
+    height: 15%;
+    color: deepskyblue;
+
+    &:hover {
+        cursor: pointer;
+    }
+`
+export const LeftCategoryItemsWrapper = styled.div`
     font-size: 1.2rem;
+    width: 60%;
+    padding-block: 10px;
+    padding-left: 15px;
+    display: flex;
+    gap: 10px;
+    justify-content: left;
+    align-items: center;
     
     &:hover {
         cursor: pointer;
-        opacity: 1;
+        font-weight: bold;
     }
 `
-export const BottomContainer = styled.div`
-    width: 100vw;
-    height: 100px;
-    background-color: black;
+export const LeftCategoryItemIcon = styled.img`
+    width: 30px;
+    height: 30px;
 `
+export const LeftCategoryItemName = styled.span`
+
+`
+
+// 상단 메뉴 탭 컨테이너
+export const TopContainer = styled.div`
+    width: 100%;
+    height: 7%;
+    display: flex;
+    justify-content: right;
+    border-bottom: 1px solid lightgray;
+`
+export const ButtonWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    padding-inline: 15px;
+`
+export const Button = styled.div`
+    border: 1px solid lightgray;
+    border-radius: 5px;
+    padding-block: 6px;
+    padding-inline: 8px;
+    font-size: 0.9rem;
+    
+    &:hover {
+        cursor: pointer;
+        background-color: lightgray;
+    }
+`
+
+// 중단 컨텐츠 내용 컨테이너 
+export const OuterContainer = styled.div`
+    width: 100%;
+    height: 100%;
+    position: relative;
+`
+
+export const BackgroundImage = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-image: url('/mju_logo.png');
+  background-repeat: no-repeat;
+  background-position: center center;
+  opacity: 0.3;
+`
+
+export const ContentContainer = styled.div`
+  position: relative;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 30px;
+`
+export const ContentTodayInformationBlock = styled.div`
+    width: 85%;
+    background-color: white;
+    font-weight: bold;
+    font-size: 1.5rem;
+    padding: 10px;
+    box-shadow: 0 6px 8px rgba(0, 0, 0, 0.1), 0 1px 3px rgba(0, 0, 0, 0.08);
+`
+export const ContentBlockLine = styled.div`
+    display: flex;
+    width: 100%;
+    justify-content: center;
+    gap: 5%;
+`
+export const ContentBlock = styled.div`
+    border-radius: 5px;
+    background-color: white;
+    width: 40%;
+    height: 35vh;
+    display: flex;
+    flex-direction: column;
+    box-shadow: 0 6px 8px rgba(0, 0, 0, 0.1), 0 1px 3px rgba(0, 0, 0, 0.08);
+`
+export const ContentLongBlock = styled.div`
+    border-radius: 5px;
+    background-color: white;
+    width: 85%;
+    height: 35vh;
+    display: flex;
+    flex-direction: column;
+    box-shadow: 0 6px 8px rgba(0, 0, 0, 0.1), 0 1px 3px rgba(0, 0, 0, 0.08);
+`
+export const BlockTitle = styled.div`
+    text-align: center;
+    padding: 15px;
+    font-weight: bold;
+    font-size: 1.2rem;
+    width: 100%;
+    border-bottom: 1px dotted lightgray;
+`
+export const BlockContent = styled.div`
+    padding-top: 5px;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    align-items: center;
+    justify-content: center;
+`
+
+// 오늘의 식단
+export const TodayFoodWrapper = styled.div`
+    text-align: center;
+`
+
+// 남은 식권 개수
+export const TodayLeftWrapper = styled.div`
+    font-size: 3rem;
+    font-weight: bold;
+`
+
+// 커뮤니티 게시글
